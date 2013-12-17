c9c0a600147e7551fe45c5242a93afba8df592a9
Henry Harrison <henry.schafer.harrison@gmail.com>
2013-12-16 23:43:22 -0500

    Improved plots

--- plots.py
+++ plots.py
@@ -12,19 +12,22 @@ angle_map = {1: 12,
              7: 45}
 data['angle'] = [angle_map[angle] for angle in data['angle']]
 
+#workaround for timedelta since pandas can't take the mean of a timedelta:
+# http://stackoverflow.com/questions/20625982/split-apply-combine-on-pandas-timedelta-column
+data['rt'] = [10**-9 * float(rt) for rt in data['rt']]
+
 means = data.groupby(['angle', 'perception']).mean()
 means['can step'] *= 100
 means = means.reset_index()
 
-can_step = means.pivot(index='angle', columns='perception', values='can step')
-confidence = means.pivot(index='angle', columns='perception', values='confidence')
 
-p = can_step.plot()
-plt.xlabel('Angle of inclination (deg)')
-plt.ylabel('Percent "Yes"')
-p.get_figure().savefig('can_step.png')
+def make_plot(values, ylabel, filename):
+    p = means.pivot(index='angle', columns='perception', values=values).plot(style='-o')
+    plt.xlabel('Angle of inclination (deg)')
+    plt.ylabel(ylabel)
+    p.get_figure().savefig(filename)
 
-p = confidence.plot()
-plt.xlabel('Angle of inclination (deg)')
-plt.ylabel('Percent "Yes"')
-p.get_figure().savefig('confidence.png')
+
+make_plot('can step', 'Percent "Yes"', 'can_step.png')
+make_plot('confidence', 'Confidence Judgment', 'confidence.png')
+make_plot('rt', 'Response Time (sec)', 'rt.png')
